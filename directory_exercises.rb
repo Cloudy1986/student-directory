e0970e1e9a23ca35829782f7d15a72b933d9b599
Mark McLeod <markmcleod45@gmail.com>
2021-10-13 16:27:01 +0100

    Step 10: Reordered the methods to be more logical

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -31,6 +31,38 @@ def input_students
   end
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
 def print_header
   puts "The students of Villians Academy"
   puts "-------------"
@@ -99,38 +131,6 @@ def print_groups
     puts people_by_group[user_input]
   else
     puts "There isn't a cohort for the month you've chosen"
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
   end
 end
 
