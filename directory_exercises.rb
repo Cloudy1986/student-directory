986bbbbcb0b10cfc13c0be563d4292adb64e00eb
Mark McLeod <markmcleod45@gmail.com>
2021-10-13 15:03:48 +0100

    Step 9: Added an interactive menu

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -105,12 +105,38 @@ def print_groups(students)
   end
 end
 
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 # nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+#students = input_students
+#print_header
+#print(students)
+#print_footer(students)
 #print_groups(students)
 #print_specific_letter(students)
 #print_less_than_12_characters(students)
 #print_loop(students)
+interactive_menu
