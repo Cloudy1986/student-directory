35d5390f2ba22b439ba424086f75c5c7d3e46cf2
Mark McLeod <markmcleod45@gmail.com>
2021-10-13 11:20:45 +0100

    Step 8 exercise 7 - Added code to ask for cohort from user, handle blank input from user, validate cohort input against months, converted cohort to a symbol

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,22 +1,30 @@
 def input_students
+  # ask for the names of students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
+  # set up an empty array
   students = []
-  # get the first name
-  name = gets.chomp
-  #while the name is not empty, repeat this code
+  # set up an array of valid months
+  months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+  # set up variable for cohort
+  cohort = ""
+  # get name from user
+  name = gets.chomp.capitalize
+  # loop while name is not empty
   while !name.empty? do
-    # get more info about students
-    puts "Enter hobbies"
-    hobby = gets.chomp
-    puts "Enter country of birth"
-    country = gets.chomp
-    # add the student hash to the array
-    students << {name: name, cohort: :november, hobby: hobby, country: country}
+    # while months doesn't include cohort, ask for the cohort and get the valid month
+    while !months.include? cohort
+      puts "Enter the month of their cohort"
+      cohort = gets.chomp.capitalize
+    end
+    # convert cohort to a symbol
+    cohort = cohort.to_sym
+    # add a hash containing name and cohort to the students array
+    students << {name: name, cohort: cohort.to_sym}
+    # output the number of students
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = gets.chomp.capitalize
   end
   # return the array of students
   students
@@ -29,14 +37,18 @@ end
 
 def print(students)
   students.each.with_index(1) do |student,index|
-    puts "#{index}. #{student[:name]} (cohort: #{student[:cohort]}, hobby: #{student[:hobby]}, country: #{student[:country]})"
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
 def print_specific_letter(students)
   puts "-------------"
   puts "Students beginning with the letter 'M':"
-  students.each do |student| puts "#{student[:name]}" if student[:name][0] == "M" || student[:name][0] == "m"
+  students.each do |student| puts "#{student[:name]}" if student[:name][0] == "M"
   end
 end
 
@@ -57,15 +69,11 @@ def print_loop(students)
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
-end
-
 # nothing happens until we call the methods
 students = input_students
 print_header
 print(students)
 print_footer(students)
-print_specific_letter(students)
-print_less_than_12_characters(students)
-print_loop(students)
+#print_specific_letter(students)
+#print_less_than_12_characters(students)
+#print_loop(students)
